8835dbaa41e8cf1a3c3a391410af94de689132a4
harshvidap <75980283+harshvidap@users.noreply.github.com>
2022-03-02 16:39:12 +0000

    Add files via upload

---
+++ C#_Hello.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ClassLibrary1;
+
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string st;
+            //Console.ReadLine(); 
+            Console.WriteLine($"Hello, {args[0]}!");
+            var myClass = new Class1
+            {
+                Age = 24,
+                Name = "Harsh"
+            };
+            Console.WriteLine(myClass.Age);
+        }
+    }
+}
---
+++ Descent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace The_Descent
+{
+    class Descent
+    {
+        static void Main(string[] args)
+        {
+
+            // game loop
+            while (true)
+            {
+                int max = 0; //Initial Maximum Height of Mountain
+                int h = 0; //Intial Mountain Index
+                for (int i = 0; i < 8; i++)
+                {
+                    int mountainH = int.Parse(Console.ReadLine());// represents the height of one mountain.
+
+                    if (mountainH > max)
+                    {
+
+                        max = mountainH; //Mountain with max height will be stored  
+                        h = i; // Mountain index will be stored
+
+                    }
+
+                }
+                Console.WriteLine(h); // Print the index of the Mountain with max Height
+
+            }
+        }
+    }
+}
